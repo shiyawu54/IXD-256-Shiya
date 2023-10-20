e3ca4a71d4ed69511d7ee4d35390ab1a01eb2692
shiyawu54 <124006593+shiyawu54@users.noreply.github.com>
2023-10-20 11:04:31 -0700

    Update main.py

--- assignment2/main.py
+++ assignment2/main.py
@@ -4,7 +4,7 @@ from js import document
 data = None
 
 def setup():
-  p5.createCanvas(300, 300)
+  p5.createCanvas(400, 400)
   print('hello p5!')
 
 def draw():
@@ -16,9 +16,22 @@ def draw():
   circle_size = int(data)
   p5.noStroke()
   p5.fill(150)
-  p5.ellipse(150, 150, circle_size, circle_size)
+  #p5.ellipse(150, 150, circle_size, circle_size)
+  p5.push()
+  # set angle variable to integer of data
+  angle = int(data)
+  # move to middle of canvas
+  p5.translate(p5.width/2, p5.height/2)
+  # rotate canvas with angle converted from degrees to radians:
+  p5.rotate(p5.radians(angle))
+  # change mode to draw rectangles from center:
+  p5.rectMode(p5.CENTER)
+  # draw rectangle at coordinate 0, 0 and 100 width and height:
+  p5.rect(0, 0, 100, 100)
+  # rectore graphical transformation:
+  p5.pop()
 
-def print_test(x):
-  print(x)
+
+
 
   
