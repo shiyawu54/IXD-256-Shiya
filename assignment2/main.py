7f6ad25c682ee8904ff07307dba04dc1a315d19a
shiyawu54 <124006593+shiyawu54@users.noreply.github.com>
2023-10-26 20:32:37 -0700

    Update main.py

--- assignment2/main.py
+++ assignment2/main.py
@@ -1,38 +1,82 @@
 import js as p5
 from js import document
 
-data = None
+data_string = None
+data_list = None
+sensor_val = None
+button_val = None
 
 def setup():
-    p5.createCanvas(400, 400)
-    print('hello p5!')
+    p5.createCanvas(800, 800)
 
 def draw():
-    p5.background(255)
+    p5.background(0)
+    global data_string, data_list
+    global sensor_val, button_val
 
-    global data
-    data = document.getElementById("data").innerText
-    
-    # Set the rotation angle based on the data
-    angle = int(data)
+    # assign content of "data" div on index.html page to variable:
+    data_string = document.getElementById("data").innerText
+    # split data_string by comma, making a list:
+    data_list = data_string.split(',')
+
+    # assign 1st item of data_list to sensor_val:
+    sensor_val = int(data_list[0])
+    # assign 2nd item of data_list to button_val:
+    button_val = int(data_list[1])
+    p5.noStroke()
+    p5.fill(0)
+    p5.text('sensor_val = ' + str(sensor_val), 10, 20)
+    p5.text('button_val = ' + str(button_val), 10, 35)
+
+    # change fill color with button value:
+    if(button_val == 0):
+        p5.fill(255)  # red fill
+    else:
+        p5.fill(200,60,60)  # white fill
+
+    # Draw rotating ellipses around a central circle
     p5.push()
     p5.translate(p5.width/2, p5.height/2)
-    p5.rotate(p5.radians(angle))
-    
-    # Draw the center of the flower
-    p5.fill(255, 204, 0)  # Yellow color for the center
-    p5.ellipse(0, 0, 50, 50)  # Center circle
+    angle_offset = sensor_val
 
-    # Draw petals around the center
-    num_petals = 8  # Number of petals
-    petal_length = 100  # Length of the petal
-    petal_width = 30  # Width of the petal
+    # Scale the flower based on button_val
+    scaling_factor = 1.5 if button_val == 1 else 1
+    p5.scale(scaling_factor)
 
-    p5.fill(255, 102, 204)  # Pink color for the petals
+    # Draw intricate petals around the center
+    num_petals = 5
+    petal_distance = 90
+    petal_length = 200
+    petal_width = 200
+
     for i in range(num_petals):
-        petal_angle = p5.TWO_PI / num_petals * i
-        x = p5.cos(petal_angle) * (25 + petal_length/2)
-        y = p5.sin(petal_angle) * (25 + petal_length/2)
-        p5.ellipse(x, y, petal_length, petal_width)
-    
+        petal_angle = p5.TWO_PI / num_petals * i + p5.radians(angle_offset)
+        x = p5.cos(petal_angle) * petal_distance
+        y = p5.sin(petal_angle) * petal_distance
+        
+        p5.push()
+        p5.translate(x, y)
+        p5.rotate(petal_angle + p5.HALF_PI)
+        
+        # Draw intricate petal with multiple bezier curves
+        p5.beginShape()
+        p5.vertex(0, -petal_length/2)
+        p5.bezierVertex(petal_width/6, -petal_length/3, petal_width/4, -petal_length/4, petal_width/3, 0)
+        p5.bezierVertex(petal_width/4, petal_length/4, petal_width/6, petal_length/3, 0, petal_length/2)
+        p5.bezierVertex(-petal_width/6, petal_length/3, -petal_width/4, petal_length/4, -petal_width/3, 0)
+        p5.bezierVertex(-petal_width/4, -petal_length/4, -petal_width/6, -petal_length/3, 0, -petal_length/2)
+        p5.endShape(p5.CLOSE)
+        
+        p5.pop()
+
+    # Draw the center of the flower with a pattern
+    p5.fill(230)
+    p5.ellipse(0, 0, 80, 80)
+    p5.fill(100,80,180)
+    for i in range(12):
+        angle = p5.TWO_PI / 12 * i
+        x = p5.cos(angle) * 30
+        y = p5.sin(angle) * 30
+        p5.ellipse(x, y, 10, 10)
+
     p5.pop()
